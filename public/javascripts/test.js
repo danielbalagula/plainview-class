522ddd1461e65852f69c0140fb8f1c2c40f7d082
Daniel <dbalagula@live.com>
2016-10-16 16:00:11 -0400

    Dagre d3 is nice

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -9,23 +9,45 @@ function drawGraph(currentDiscussionId){
 
     d3.json('http://localhost:3000/api/discussions/id/' + currentDiscussionId, function(discussion){
 
-    	var data = [];
-    	var svg = d3.select("#debateExperience").attr("width", "100%").attr("height", "100%");
-    	var x = 20;
+    	// var data = [];
+    	// var svg = d3.select("#debateExperience").attr("width", "100%").attr("height", "100%");
+
+		var g = new dagreD3.graphlib.Graph()
+		  .setGraph({})
+		  .setDefaultEdgeLabel(function() { return {}; });
 
     	discussion.responses.forEach(function(response){
-    		
-			var responseNode = svg.append("foreignObject").attr("x", 50).attr("y", 15).attr("width", 200).attr("id", "n"+response._id).attr("stroke", "black");
-			var responseText =  responseNode.append("xhtml:div").attr("style", "id:t" + response._id + "; width:190px; height:auto; overflow-y:auto").text(response.text);
-			var textHeight = responseNode[0][0].childNodes[0].clientHeight;
-			responseNode.attr("height", textHeight + 10)
-			svg.append("rect").attr("x",responseNode.attr("x")-10).attr("y",responseNode.attr("y")-10).attr("width",200).attr("height", textHeight+15).attr("fill","none").attr("stroke", "black")
-			// console.log($("#t" + response._id));
-			// console.log($("n"+response._id).css("height"))
 
-    		data.push({"overflow-y": "auto","id": response._id , "fill": "white", "x": x, "width":80, "height":80, "label": response.text});
-    		x += 100;
+    		g.setNode("1",  { label: response.text});
+    		g.setNode("2",  { label: response.text});
+    		g.setNode(3,  { label: response.text});
+    		g.setNode(4,  { label: response.text});
+
     	});
+    	g.setEdge("1", "2");
+
+    	g.nodes().forEach(function(v) {
+		  var node = g.node(v);
+		  node.rx = node.ry = 5;
+		});
+
+		var svg = d3.select("svg"),
+			inner = svg.select("g");
+		    svgGroup = svg.append("g");
+
+
+		var zoom = d3.behavior.zoom().on("zoom", function() {
+		      inner.attr("transform", "translate(" + d3.event.translate + ")" +
+		                                  "scale(" + d3.event.scale + ")");
+		    });
+		svg.call(zoom);
+
+		var render = new dagreD3.render();
+
+		render(d3.select("svg g"), g);
+
+		var xCenterOffset = (svg.attr("width") - g.graph().width) / 2;
+		svgGroup.attr("transform", "translate(" + xCenterOffset + ", 20)");
     	
 
    //  	var elem = svg.selectAll("g myCircleText")
