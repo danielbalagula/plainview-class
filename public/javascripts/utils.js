c9edd7eed88903bfc49265b434b63360d6baa155
Daniel <dbalagula@live.com>
2016-11-05 12:26:53 -0400

    jumbotrun

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -2,4 +2,13 @@ $(document).ready(function(){
 	$(function () {
 	  $('[data-toggle="tooltip"]').tooltip()
 	})
+	var jumboHeight = $('.jumbotron').outerHeight();
+	function parallax(){
+	    var scrolled = $(window).scrollTop();
+	    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+	}
+
+	$(window).scroll(function(e){
+	    parallax();
+	});
 })
