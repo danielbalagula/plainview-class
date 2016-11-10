afc073608b20e2fd54eb353067e3ae8b03ebc062
danielbalagula <db2791@nyu.edu>
2016-11-10 12:51:16 -0500

    Added NewRelic monitoring

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Plainview'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'c9d491aa5709dc3fe8a811ee48bca75ba4e6f403',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
