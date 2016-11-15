e591e1ee39a9354e9bc2a7c3d753d129363efa85
Daniel Balagula <danielbalagula@live.com>
2016-11-14 19:52:59 -0500

    prez site

---
+++ public/js/pres.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	var startedSlideShow = false;
+
+	$(document).on('scroll', function() {
+	    if($(this).scrollTop()>=$('#features').position().top){
+	        if (startedSlideShow === false){
+	        	var slideIndex = 0;
+				carousel();
+				startedSlideShow = true;
+
+				function carousel() {
+				    var i;
+				    var x = document.getElementsByClassName("convoslides");
+				    for (i = 0; i < x.length; i++) {
+				      x[i].style.display = "none"; 
+				    }
+				    slideIndex++;
+				    if (slideIndex > x.length) {slideIndex = 1} 
+				    x[slideIndex-1].style.display = "block"; 
+				    setTimeout(carousel, 1250); // Change image every 2 seconds
+				}
+	        }
+	    }
+	})
+})
